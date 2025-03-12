34dd6c19ed86a7e599b8384c4d19259d3c165e12
Dish Dudhat <dishdudhat85@gmail.com>
2025-03-11 21:18:51 -0400

    updated file with new changes

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
+
 app.use(expressLayouts);
 app.use(express.urlencoded({ extended: true })); // Parse form data
 
